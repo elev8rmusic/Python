5baf14552c7ca20b2a8fd87165051476a9abbaac
Brian Holden <141460538+BrianHolden0@users.noreply.github.com>
2023-08-04 16:19:18 -0700

    Create exobiology1.py

---
+++ exobiology1.py
@@ -0,0 +1,353 @@
+'''
+Created 20230804
+@author: BrianHolden0
+'''
+
+from exobiology1 import exochoice
+
+
+
+def exovalue(self):
+    global Total
+    ### Aleoida
+    if input2 == 1:
+        print("You added Aleoida Arcus")
+        Total = Total + 7252500
+    elif input2 == 2:
+        print("You added Aleoida Coronamus")
+        Total = Total + 6284600
+    elif input2 == 3:
+        print("You added Aleoida Gravis")
+        Total = Total + 12934900
+    elif input2 == 4:
+        print("You added Aleoida Laminiae")
+        Total = Total + 3385200
+    elif input2 == 5:
+        print("You added Aleoida Spica")
+        Total = Total + 3385200
+    ### Bacterium
+    elif input2 == 6:
+        print("You added Bacterium Acies")
+        Total = Total + 1000000
+    elif input2 == 7:
+        print("You added Bacterium Alcyoneum")
+        Total = Total + 1658500
+    elif input2 == 8:
+        print("You added Bacterium Aurasus")
+        Total = Total + 1000000
+    elif input2 == 9:
+        print("You added Bacterium Bullaris")
+        Total = Total + 1152500
+    elif input2 == 10:
+        print("You added Bacterium Cerbrus")
+        Total = Total + 1689800
+    elif input2 == 11:
+        print("You added Bacterium Informem")
+        Total = Total + 8418000
+    elif input2 == 12:
+        print("You added Bacterium Nebulus")
+        Total = Total + 5289900
+    elif input2 == 13:
+        print("You added Bacterium Omentum")
+        Total = Total + 4638900
+    elif input2 == 14:
+        print("You added Bacterium Scopulum")
+        Total = Total + 4934500
+    elif input2 == 15:
+        print("You added Bacterium Tela")
+        Total = Total + 1949000
+    elif input2 == 16:
+        print("You added Bacterium Verrata")
+        Total = Total + 3897000
+    elif input2 == 17:
+        print("You added Bacterium Vesicula")
+        Total = Total + 1000000
+    elif input2 == 18:
+        print("You added Bacterium Volu")
+        Total = Total + 7774700
+    ### Cactoida
+    elif input2 == 19:
+        print("You added Cactoida Cortexum")
+        Total = Total + 3667600
+    elif input2 == 20:
+        print("You added Cactoida Lapis")
+        Total = Total + 2483600
+    elif input2 == 21:
+        print("You added Cactoida Peperatis")
+        Total = Total + 2483600
+    elif input2 == 22:
+        print("You added Cactoida Pullulanta")
+        Total = Total + 3667600
+    elif input2 == 23:
+        print("You added Cactoida Vermis")
+        Total = Total + 16202800
+    ### Clypeus
+    elif input2 == 24:
+        print("You added Clypeus Lacrimam")
+        Total = Total + 8418000
+    elif input2 == 25:
+        print("You added Clypeus Margaritus")
+        Total = Total + 11873200
+    elif input2 == 26:
+        print("You added Clypeus Speculumi")
+        Total = Total + 16202800
+    ### Concha
+    elif input2 == 27:
+        print("You added Concha Aureolas")
+        Total = Total + 7774700
+    elif input2 == 28:
+        print("You added Concha Biconcavis")
+        Total = Total + 19010800
+    elif input2 == 29:
+        print("You added Concha Labiata")
+        Total = Total + 2352400
+    elif input2 == 30:
+        print("You added Concha Renibus")
+        Total = Total + 4572400
+    ### Electricae
+    elif input2 == 31:
+        print("You added Electricae Pluma")
+        Total = Total + 6284600
+    elif input2 == 32:
+        print("You added Electricae Radialem")
+        Total = Total + 6284600
+    ### Fonticulua
+    elif input2 == 33:
+        print("You added Fonticulua Campestris")
+        Total = Total + 1000000
+    elif input2 == 34:
+        print("You added Fonticulua Digitos")
+        Total = Total + 1804100
+    elif input2 == 35:
+        print("You added Fonticulua Fluctus")
+        Total = Total + 20000000
+    elif input2 == 36:
+        print("You added Fonticulua Lapida")
+        Total = Total + 3111000
+    elif input2 == 37:
+        print("You added Fonticulua Segmentatus")
+        Total = Total + 19010800
+    elif input2 == 38:
+        print("You added Fonticulua Upupam")
+        Total = Total + 5727600
+    ### Frutexa
+    elif input2 == 39:
+        print("You added Frutexa Acus")
+        Total = Total + 7774700
+    elif input2 == 40:
+        print("You added Frutexa Collum")
+        Total = Total + 1639800
+    elif input2 == 41:
+        print("You added Frutexa Fera")
+        Total = Total + 1632500
+    elif input2 == 42:
+        print("You added Frutexa Flabellum")
+        Total = Total + 1808900
+    elif input2 == 43:
+        print("You added Frutexa Flammasis")
+        Total = Total + 10326000
+    elif input2 == 44:
+        print("You added Frutexa Metallicum")
+        Total = Total + 1632500
+    elif input2 == 45:
+        print("You added Frutexa Sponsae")
+        Total = Total + 5988000
+    ### Fumerola
+    elif input2 == 46:
+        print("You added Fumerola Aquatis")
+        Total = Total + 6284600
+    elif input2 == 47:
+        print("You added Fumerola Carbosis")
+        Total = Total + 6284600
+    elif input2 == 48:
+        print("You added Fumerola Extremus")
+        Total = Total + 16202800
+    elif input2 == 49:
+        print("You added Fumerolaa Nitris")
+        Total = Total + 7500900
+    ### Fungoida
+    elif input2 == 50:
+        print("You added Fungoida Bullarum")
+        Total = Total + 3703200
+    elif input2 == 51:
+        print("You added Fungoida Gelata")
+        Total = Total + 3330300
+    elif input2 == 52:
+        print("You added Fungoida Setisis")
+        Total = Total + 1670100
+    elif input2 == 53:
+        print("You added Fungoida Stabitis")
+        Total = Total + 2680300
+    ### Osseus
+    elif input2 == 54:
+        print("You added Osseus Cornibus")
+        Total = Total + 1483000
+    elif input2 == 55:
+        print("You added Osseus Discus")
+        Total = Total + 12934900
+    elif input2 == 56:
+        print("You added Osseus Fractus")
+        Total = Total + 4027800
+    elif input2 == 57:
+        print("You added Osseus Pellebantus")
+        Total = Total + 9739000
+    elif input2 == 58:
+        print("You added Osseus Pumice")
+        Total = Total + 3156300
+    elif input2 == 59:
+        print("You added Osseus Spiralis")
+        Total = Total + 2404700
+    ### Recepta
+    elif input2 == 60:
+        print("You added Recepta Conditivus")
+        Total = Total + 14313700
+    elif input2 == 61:
+        print("You added Recepta Deltahedronix")
+        Total = Total + 16202800
+    elif input2 == 62:
+        print("You added Recepta Umbrux")
+        Total = Total + 12934900
+    ### Stratum
+    elif input2 == 63:
+        print("You added Stratum Araneamus")
+        Total = Total + 2448900
+    elif input2 == 64:
+        print("You added Stratum Cucumisis")
+        Total = Total + 16202800
+    elif input2 == 65:
+        print("You added Stratum Excutitus")
+        Total = Total + 2448900
+    elif input2 == 66:
+        print("You added Stratum Frigus")
+        Total = Total + 2637500
+    elif input2 == 67:
+        print("You added Stratum Laminamus")
+        Total = Total + 2788300
+    elif input2 == 68:
+        print("You added Stratum Limaxus")
+        Total = Total + 1362000
+    elif input2 == 69:
+        print("You added Stratum Paleas")
+        Total = Total + 1362000
+    elif input2 == 70:
+        print("You added Stratum Tectonicas")
+        Total = Total + 19010800
+    ### Tubus
+    elif input2 == 71:
+        print("You added Tubus Cavas")
+        Total = Total + 11873200
+    elif input2 == 72:
+        print("You added Tubus Compagibus")
+        Total = Total + 7774700
+    elif input2 == 73:
+        print("You added Tubus Conifer")
+        Total = Total + 2415500
+    elif input2 == 74:
+        print("You added Tubus Rosarium")
+        Total = Total + 2637500
+    elif input2 == 75:
+        print("You added Tubus Sororibus")
+        Total = Total + 5727600
+    ### Tussock
+    elif input2 == 76:
+        print("You added Tussock Albata")
+        Total = Total + 3252500
+    elif input2 == 77:
+        print("You added Tussock Capillum")
+        Total = Total + 7025800
+    elif input2 == 78:
+        print("You added Tussock Caputus")
+        Total = Total + 3472400
+    elif input2 == 79:
+        print("You added Tussock Catena")
+        Total = Total + 1766600
+    elif input2 == 80:
+        print("You added Tussock Cultro")
+        Total = Total + 1766600
+    elif input2 == 81:
+        print("You added Tussock Divisa")
+        Total = Total + 1766600
+    elif input2 == 82:
+        print("You added Tussock Ignis")
+        Total = Total + 1849000
+    elif input2 == 83:
+        print("You added Tussock Pennata")
+        Total = Total + 5853800
+    elif input2 == 84:
+        print("You added Tussock Pennatis")
+        Total = Total + 1000000
+    elif input2 == 85:
+        print("You added Tussock Propagito")
+        Total = Total + 1000000
+    elif input2 == 86:
+        print("You added Tussock Serrati")
+        Total = Total + 4447100
+    elif input2 == 87:
+        print("You added Tussock Stigmasis")
+        Total = Total + 19010800
+    elif input2 == 88:
+        print("You added Tussock Triticum")
+        Total = Total + 7774700
+    elif input2 == 89:
+        print("You added Tussock Ventusa")
+        Total = Total + 3227700
+    elif input2 == 90:
+        print("You added Tussock Virgam")
+        Total = Total + 14313700
+    else: pass
+
+def continuequestionmark(input3):
+    global Flag
+    if input3.upper() == "Y":
+        Flag = Flag + 1
+    elif input3.upper() == "N":
+        print(" ")
+        firstdiscoveredquestionmark()
+        print("Assuming 100% of the exobiology was already discovered, the grand total for all " + str(Flag) + " samples that you entered is:")
+        print(Total)
+        print("o7 CMDR")
+        Flag = 0
+
+def firstdiscoveredquestionmark():
+    import sys
+    global Total
+    for retry in range(4):
+        input4 = input("Were you the first to discover ALL of the submitted exobiology? (Y/N) ")
+        if input4.upper() == "Y":
+            Total = Total * 5
+            break
+        elif input4.upper() == "N":
+            break
+        print("Please input an acceptable answer")
+    else:
+        for retry in range(1):
+            input4 = input("Were you the first to discover ALL of the submitted exobiology? (Y/N) ")
+            if input4.upper() == "Y":
+                Total = Total * 5
+                break
+            elif input4.upper() == "N":
+                break
+            print("How dare you! Too many incorrect inputs. Exiting the program...")
+        else: sys.exit(1)
+
+Total = 0
+Flag = 1
+while Flag != 0:
+    print(" ")
+    print("------------------------------OPTIONS------------------------------")
+    print("1=Aleoida      2=Bacterium    3=Cactoida   4=Clypeus    5=Concha")
+    print("6=Electricae   7=Fonticulua   8=Frutexa    9=Fumerola   10=Fungoida")
+    print("11=Osseus      12=Recepta     13=Stratum   14=Tubus     15=Tussock")
+    print("------------------------------OPTIONS------------------------------")
+    input1 = int(input("Enter the number associated with your exobiology's genus: "))
+    exochoice(input1)
+    print(" ")
+    input2 = int(input("Enter the number associated with your exobiology's species: "))
+    exovalue(input2)
+    print("The current running total is " + str(Total) + " credits.")
+    print(" ")
+    input3 = str(input("Would you like to add more exobiology? (Y/N): "))
+    continuequestionmark(input3)
+
+###Idea for the end: use a list to show all of the exobiology that the user entered and display it with "\n" before
+### each line of output so it's nice and clean.
