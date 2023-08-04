28319f51d168007cd5f1630123401a34b2e66fae
Brian Holden <141460538+BrianHolden0@users.noreply.github.com>
2023-08-04 16:23:12 -0700

    Update exobiology1.py

--- exobiology1.py
+++ exobiology1.py
@@ -3,351 +3,140 @@ Created 20230804
 @author: BrianHolden0
 '''
 
-from exobiology1 import exochoice
 
-
-
-def exovalue(self):
-    global Total
-    ### Aleoida
-    if input2 == 1:
-        print("You added Aleoida Arcus")
-        Total = Total + 7252500
-    elif input2 == 2:
-        print("You added Aleoida Coronamus")
-        Total = Total + 6284600
-    elif input2 == 3:
-        print("You added Aleoida Gravis")
-        Total = Total + 12934900
-    elif input2 == 4:
-        print("You added Aleoida Laminiae")
-        Total = Total + 3385200
-    elif input2 == 5:
-        print("You added Aleoida Spica")
-        Total = Total + 3385200
-    ### Bacterium
-    elif input2 == 6:
-        print("You added Bacterium Acies")
-        Total = Total + 1000000
-    elif input2 == 7:
-        print("You added Bacterium Alcyoneum")
-        Total = Total + 1658500
-    elif input2 == 8:
-        print("You added Bacterium Aurasus")
-        Total = Total + 1000000
-    elif input2 == 9:
-        print("You added Bacterium Bullaris")
-        Total = Total + 1152500
-    elif input2 == 10:
-        print("You added Bacterium Cerbrus")
-        Total = Total + 1689800
-    elif input2 == 11:
-        print("You added Bacterium Informem")
-        Total = Total + 8418000
-    elif input2 == 12:
-        print("You added Bacterium Nebulus")
-        Total = Total + 5289900
-    elif input2 == 13:
-        print("You added Bacterium Omentum")
-        Total = Total + 4638900
-    elif input2 == 14:
-        print("You added Bacterium Scopulum")
-        Total = Total + 4934500
-    elif input2 == 15:
-        print("You added Bacterium Tela")
-        Total = Total + 1949000
-    elif input2 == 16:
-        print("You added Bacterium Verrata")
-        Total = Total + 3897000
-    elif input2 == 17:
-        print("You added Bacterium Vesicula")
-        Total = Total + 1000000
-    elif input2 == 18:
-        print("You added Bacterium Volu")
-        Total = Total + 7774700
-    ### Cactoida
-    elif input2 == 19:
-        print("You added Cactoida Cortexum")
-        Total = Total + 3667600
-    elif input2 == 20:
-        print("You added Cactoida Lapis")
-        Total = Total + 2483600
-    elif input2 == 21:
-        print("You added Cactoida Peperatis")
-        Total = Total + 2483600
-    elif input2 == 22:
-        print("You added Cactoida Pullulanta")
-        Total = Total + 3667600
-    elif input2 == 23:
-        print("You added Cactoida Vermis")
-        Total = Total + 16202800
-    ### Clypeus
-    elif input2 == 24:
-        print("You added Clypeus Lacrimam")
-        Total = Total + 8418000
-    elif input2 == 25:
-        print("You added Clypeus Margaritus")
-        Total = Total + 11873200
-    elif input2 == 26:
-        print("You added Clypeus Speculumi")
-        Total = Total + 16202800
-    ### Concha
-    elif input2 == 27:
-        print("You added Concha Aureolas")
-        Total = Total + 7774700
-    elif input2 == 28:
-        print("You added Concha Biconcavis")
-        Total = Total + 19010800
-    elif input2 == 29:
-        print("You added Concha Labiata")
-        Total = Total + 2352400
-    elif input2 == 30:
-        print("You added Concha Renibus")
-        Total = Total + 4572400
-    ### Electricae
-    elif input2 == 31:
-        print("You added Electricae Pluma")
-        Total = Total + 6284600
-    elif input2 == 32:
-        print("You added Electricae Radialem")
-        Total = Total + 6284600
-    ### Fonticulua
-    elif input2 == 33:
-        print("You added Fonticulua Campestris")
-        Total = Total + 1000000
-    elif input2 == 34:
-        print("You added Fonticulua Digitos")
-        Total = Total + 1804100
-    elif input2 == 35:
-        print("You added Fonticulua Fluctus")
-        Total = Total + 20000000
-    elif input2 == 36:
-        print("You added Fonticulua Lapida")
-        Total = Total + 3111000
-    elif input2 == 37:
-        print("You added Fonticulua Segmentatus")
-        Total = Total + 19010800
-    elif input2 == 38:
-        print("You added Fonticulua Upupam")
-        Total = Total + 5727600
-    ### Frutexa
-    elif input2 == 39:
-        print("You added Frutexa Acus")
-        Total = Total + 7774700
-    elif input2 == 40:
-        print("You added Frutexa Collum")
-        Total = Total + 1639800
-    elif input2 == 41:
-        print("You added Frutexa Fera")
-        Total = Total + 1632500
-    elif input2 == 42:
-        print("You added Frutexa Flabellum")
-        Total = Total + 1808900
-    elif input2 == 43:
-        print("You added Frutexa Flammasis")
-        Total = Total + 10326000
-    elif input2 == 44:
-        print("You added Frutexa Metallicum")
-        Total = Total + 1632500
-    elif input2 == 45:
-        print("You added Frutexa Sponsae")
-        Total = Total + 5988000
-    ### Fumerola
-    elif input2 == 46:
-        print("You added Fumerola Aquatis")
-        Total = Total + 6284600
-    elif input2 == 47:
-        print("You added Fumerola Carbosis")
-        Total = Total + 6284600
-    elif input2 == 48:
-        print("You added Fumerola Extremus")
-        Total = Total + 16202800
-    elif input2 == 49:
-        print("You added Fumerolaa Nitris")
-        Total = Total + 7500900
-    ### Fungoida
-    elif input2 == 50:
-        print("You added Fungoida Bullarum")
-        Total = Total + 3703200
-    elif input2 == 51:
-        print("You added Fungoida Gelata")
-        Total = Total + 3330300
-    elif input2 == 52:
-        print("You added Fungoida Setisis")
-        Total = Total + 1670100
-    elif input2 == 53:
-        print("You added Fungoida Stabitis")
-        Total = Total + 2680300
-    ### Osseus
-    elif input2 == 54:
-        print("You added Osseus Cornibus")
-        Total = Total + 1483000
-    elif input2 == 55:
-        print("You added Osseus Discus")
-        Total = Total + 12934900
-    elif input2 == 56:
-        print("You added Osseus Fractus")
-        Total = Total + 4027800
-    elif input2 == 57:
-        print("You added Osseus Pellebantus")
-        Total = Total + 9739000
-    elif input2 == 58:
-        print("You added Osseus Pumice")
-        Total = Total + 3156300
-    elif input2 == 59:
-        print("You added Osseus Spiralis")
-        Total = Total + 2404700
-    ### Recepta
-    elif input2 == 60:
-        print("You added Recepta Conditivus")
-        Total = Total + 14313700
-    elif input2 == 61:
-        print("You added Recepta Deltahedronix")
-        Total = Total + 16202800
-    elif input2 == 62:
-        print("You added Recepta Umbrux")
-        Total = Total + 12934900
-    ### Stratum
-    elif input2 == 63:
-        print("You added Stratum Araneamus")
-        Total = Total + 2448900
-    elif input2 == 64:
-        print("You added Stratum Cucumisis")
-        Total = Total + 16202800
-    elif input2 == 65:
-        print("You added Stratum Excutitus")
-        Total = Total + 2448900
-    elif input2 == 66:
-        print("You added Stratum Frigus")
-        Total = Total + 2637500
-    elif input2 == 67:
-        print("You added Stratum Laminamus")
-        Total = Total + 2788300
-    elif input2 == 68:
-        print("You added Stratum Limaxus")
-        Total = Total + 1362000
-    elif input2 == 69:
-        print("You added Stratum Paleas")
-        Total = Total + 1362000
-    elif input2 == 70:
-        print("You added Stratum Tectonicas")
-        Total = Total + 19010800
-    ### Tubus
-    elif input2 == 71:
-        print("You added Tubus Cavas")
-        Total = Total + 11873200
-    elif input2 == 72:
-        print("You added Tubus Compagibus")
-        Total = Total + 7774700
-    elif input2 == 73:
-        print("You added Tubus Conifer")
-        Total = Total + 2415500
-    elif input2 == 74:
-        print("You added Tubus Rosarium")
-        Total = Total + 2637500
-    elif input2 == 75:
-        print("You added Tubus Sororibus")
-        Total = Total + 5727600
-    ### Tussock
-    elif input2 == 76:
-        print("You added Tussock Albata")
-        Total = Total + 3252500
-    elif input2 == 77:
-        print("You added Tussock Capillum")
-        Total = Total + 7025800
-    elif input2 == 78:
-        print("You added Tussock Caputus")
-        Total = Total + 3472400
-    elif input2 == 79:
-        print("You added Tussock Catena")
-        Total = Total + 1766600
-    elif input2 == 80:
-        print("You added Tussock Cultro")
-        Total = Total + 1766600
-    elif input2 == 81:
-        print("You added Tussock Divisa")
-        Total = Total + 1766600
-    elif input2 == 82:
-        print("You added Tussock Ignis")
-        Total = Total + 1849000
-    elif input2 == 83:
-        print("You added Tussock Pennata")
-        Total = Total + 5853800
-    elif input2 == 84:
-        print("You added Tussock Pennatis")
-        Total = Total + 1000000
-    elif input2 == 85:
-        print("You added Tussock Propagito")
-        Total = Total + 1000000
-    elif input2 == 86:
-        print("You added Tussock Serrati")
-        Total = Total + 4447100
-    elif input2 == 87:
-        print("You added Tussock Stigmasis")
-        Total = Total + 19010800
-    elif input2 == 88:
-        print("You added Tussock Triticum")
-        Total = Total + 7774700
-    elif input2 == 89:
-        print("You added Tussock Ventusa")
-        Total = Total + 3227700
-    elif input2 == 90:
-        print("You added Tussock Virgam")
-        Total = Total + 14313700
-    else: pass
-
-def continuequestionmark(input3):
-    global Flag
-    if input3.upper() == "Y":
-        Flag = Flag + 1
-    elif input3.upper() == "N":
+def exochoice(input1):
+    if input1 == 1:
         print(" ")
-        firstdiscoveredquestionmark()
-        print("Assuming 100% of the exobiology was already discovered, the grand total for all " + str(Flag) + " samples that you entered is:")
-        print(Total)
-        print("o7 CMDR")
-        Flag = 0
-
-def firstdiscoveredquestionmark():
-    import sys
-    global Total
-    for retry in range(4):
-        input4 = input("Were you the first to discover ALL of the submitted exobiology? (Y/N) ")
-        if input4.upper() == "Y":
-            Total = Total * 5
-            break
-        elif input4.upper() == "N":
-            break
-        print("Please input an acceptable answer")
-    else:
-        for retry in range(1):
-            input4 = input("Were you the first to discover ALL of the submitted exobiology? (Y/N) ")
-            if input4.upper() == "Y":
-                Total = Total * 5
-                break
-            elif input4.upper() == "N":
-                break
-            print("How dare you! Too many incorrect inputs. Exiting the program...")
-        else: sys.exit(1)
-
-Total = 0
-Flag = 1
-while Flag != 0:
-    print(" ")
-    print("------------------------------OPTIONS------------------------------")
-    print("1=Aleoida      2=Bacterium    3=Cactoida   4=Clypeus    5=Concha")
-    print("6=Electricae   7=Fonticulua   8=Frutexa    9=Fumerola   10=Fungoida")
-    print("11=Osseus      12=Recepta     13=Stratum   14=Tubus     15=Tussock")
-    print("------------------------------OPTIONS------------------------------")
-    input1 = int(input("Enter the number associated with your exobiology's genus: "))
-    exochoice(input1)
-    print(" ")
-    input2 = int(input("Enter the number associated with your exobiology's species: "))
-    exovalue(input2)
-    print("The current running total is " + str(Total) + " credits.")
-    print(" ")
-    input3 = str(input("Would you like to add more exobiology? (Y/N): "))
-    continuequestionmark(input3)
-
-###Idea for the end: use a list to show all of the exobiology that the user entered and display it with "\n" before
-### each line of output so it's nice and clean.
+        print("Aleoida Arcus-----------1")
+        print("Aleoida Coronamus-------2")
+        print("Aleoida Gravis----------3")
+        print("Aleoida Laminiae--------4")
+        print("Aleoida Spica-----------5")
+    else: pass
+    if input1 == 2:
+        print(" ")
+        print("Bacterium Acies---------6")
+        print("Bacterium Alcyoneum-----7")
+        print("Bacterium Aurasus-------8")
+        print("Bacterium Bullaris------9")
+        print("Bacterium Cerbrus-------10")
+        print("Bacterium Informem------11")
+        print("Bacterium Nebulus-------12")
+        print("Bacterium Omentum-------13")
+        print("Bacterium Scopulum------14")
+        print("Bacterium Tela----------15")
+        print("Bacterium Verrata-------16")
+        print("Bacterium Vesicula------17")
+        print("Bacterium Volu----------18")
+    else: pass
+    if input1 == 3:
+        print(" ")
+        print("Cactoida Cortexum-------19")
+        print("Cactoida Lapis----------20")
+        print("Cactoida Peperatis------21")
+        print("Cactoida Pullulanta-----22")
+        print("Cactoida Vermis---------23")
+    else: pass
+    if input1 == 4:
+        print(" ")
+        print("Clypeus Lacrimam--------24")
+        print("Clypeus Margaritus------25")
+        print("Clypeus Speculumi-------26")
+    else: pass
+    if input1 == 5:
+        print(" ")
+        print("Concha Aureolas---------27")
+        print("Concha Biconcavis-------28")
+        print("Concha Labiata----------29")
+        print("Concha Renibus----------30")
+    else: pass
+    if input1 == 6:
+        print(" ")
+        print("Electricae Pluma--------31")
+        print("Electricae Radialem-----32")
+    else: pass
+    if input1 == 7:
+        print(" ")
+        print("Fonticulua Campestris---33")
+        print("Fonticulua Digitos------34")
+        print("Fonticulua Fluctus------35")
+        print("Fonticulua Lapida-------36")
+        print("Fonticulua Segmentatus--37")
+        print("Fonticulua Upupam-------38")
+    else: pass
+    if input1 == 8:
+        print(" ")
+        print("Frutexa Acus------------39")
+        print("Frutexa Collum----------40")
+        print("Frutexa Fera------------41")
+        print("Frutexa Flabellum-------42")
+        print("Frutexa Flammasis-------43")
+        print("Frutexa Metallicum------44")
+        print("Frutexa Sponsae---------45")
+    else: pass
+    if input1 == 9:
+        print(" ")
+        print("Fumerola Aquatis--------46")
+        print("Fumerola Carbosis-------47")
+        print("Fumerola Extremus-------48")
+        print("Fumerola Nitris---------49")
+    else: pass
+    if input1 == 10:
+        print(" ")
+        print("Fugoida Bullarum--------50")
+        print("Fugoida Gelata----------51")
+        print("Fugoida Setisis---------52")
+        print("Fugoida Stabitis--------53")
+    else: pass
+    if input1 == 11:
+        print(" ")
+        print("Osseus Cornibus---------54")
+        print("Osseus Discus-----------55")
+        print("Osseus Fractus----------56")
+        print("Osseus Pellebantus------57")
+        print("Osseus Pumice-----------58")
+        print("Osseus Spiralis---------59")
+    else: pass
+    if input1 == 12:
+        print(" ")
+        print("Recepta Conditivus------60")
+        print("Recepta Deltahedronix---61")
+        print("Recepta Umbrux----------62")
+    else: pass
+    if input1 == 13:
+        print(" ")
+        print("Stratum Araneamus-------63")
+        print("Stratum Cucumisis-------64")
+        print("Stratum Excutitus-------65")
+        print("Stratum Frigus----------66")
+        print("Stratum Laminamus-------67")
+        print("Stratum Limaxus---------68")
+        print("Stratum Paleas----------69")
+        print("Stratum Tectonicas------70")
+    else: pass
+    if input1 == 14:
+        print(" ")
+        print("Tubus Cavas-------------71")
+        print("Tubus Compagibus--------72")
+        print("Tubus Conifer-----------73")
+        print("Tubus Rosarium----------74")
+        print("Tubus Sororibus---------75")
+    else: pass
+    if input1 == 15:
+        print(" ")
+        print("Tussock Albata----------76")
+        print("Tussock Capillum--------77")
+        print("Tussock Caputus---------78")
+        print("Tussock Catena----------79")
+        print("Tussock Cultro----------80")
+        print("Tussock Divisa----------81")
+        print("Tussock Ignis-----------82")
+        print("Tussock Pennata---------83")
+        print("Tussock Pennatis--------84")
+        print("Tussock Propagito-------85")
+        print("Tussock Serrati---------86")
+        print("Tussock Stigmasis-------87")
+        print("Tussock Triticum--------88")
+        print("Tussock Ventusa---------89")
+        print("Tussock Virgam----------90")
+    else: pass
